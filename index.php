c706fd4ab08358011fdc89f9f8995ce3a1853ef7
Lucas Giori Cesconetto <lucasgioricesconetto@gmail.com>
2021-07-17 17:27:07 -0300

    refactor: pwa code

--- index.php
+++ index.php
@@ -16,9 +16,36 @@
     <header>
         <img src="imgs/logo.png" alt="Vida de programador" title="Vida de programador">
     </header>
+    <div id="alert">
+        <p>Deseja adicionar este App na tela do seu dispositivo?</p>
+        <button type="button" class="btn" id="btnAdd">Adicionar a tela</button>
+        <button type="button" class="btn" id="btnClose">Fechar</button>
+    </div>
     <main id="tirinhas"> 
-    
     </main>
 </body>
 <script src="js/tirinhas.js"></script>
+<script src="js/funcoes.js"></script>
+<script>
+    if('serviceWorker' in navigator) {
+        
+        navigator.serviceWorker.register(
+            "serviceWorker.js",
+            {
+                scope: "/"
+            },
+        ).then((register) => {
+            if ( register.installing )
+                console.log('Instalado com sucesso')
+            else if ( register.waiting )
+                console.log('SW em espera')
+            else if ( register.active )
+                console.log('SW ativo')
+
+			console.log('SW registrado no escopo: '+register.scope)
+        }).catch((error) => {
+            console.log(`Erro ao tentar registrar Service Worker! Erro: ${error}`)
+        });
+    }
+</script>
 </html>
